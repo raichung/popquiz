df6a624bd3852b08c8b83d1c382ec0bd8063d7c6
Raichung <raichung.magar@gmail.com>
2022-04-07 14:35:02 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import questions from './components/questions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
---
+++ src/components/questions.js
@@ -0,0 +1,94 @@
+const questions = [
+    {
+        questionText: 'What is the capital of France?',
+        answerOptions: [
+            { answerText: 'New York', isCorrect: false },
+            { answerText: 'London', isCorrect: false },
+            { answerText: 'Paris', isCorrect: true },
+            { answerText: 'Dublin', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who is CEO of Tesla?',
+        answerOptions: [
+            { answerText: 'Jeff Bezos', isCorrect: false },
+            { answerText: 'Elon Musk', isCorrect: true },
+            { answerText: 'Bill Gates', isCorrect: false },
+            { answerText: 'Tony Stark', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What was Disney\' first movie?',
+        answerOptions: [
+            { answerText: 'Snow White', isCorrect: true },
+            { answerText: 'Cinderella', isCorrect: false },
+            { answerText: 'Pinocchio', isCorrect: false },
+            { answerText: 'The Little Mermaid', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many Harry Potter books are there?',
+        answerOptions: [
+            { answerText: '1', isCorrect: false },
+            { answerText: '4', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '7', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'When was Netflix founded?',
+        answerOptions: [
+            { answerText: '1997', isCorrect: true },
+            { answerText: '1999', isCorrect: false },
+            { answerText: '2001', isCorrect: false },
+            { answerText: '2011', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'It took Mariah Carey 15 minutes to write which holiday hit in 1994?',
+        answerOptions: [
+            { answerText: 'Zombie', isCorrect: false },
+            { answerText: 'Another Night', isCorrect: false },
+            { answerText: 'All I Want for Christmas is You', isCorrect: true },
+            { answerText: 'Can You Feel the Love Tonight', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What is the national flower of Japan?',
+        answerOptions: [
+            { answerText: 'Cherry Blossom', isCorrect: true },
+            { answerText: 'Sakura', isCorrect: false },
+            { answerText: 'Iris', isCorrect: false },
+            { answerText: 'Japanese Camellia', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which country has the most islands in the world?',
+        answerOptions: [
+            { answerText: 'Indonesia', isCorrect: false },
+            { answerText: 'Philippines', isCorrect: false },
+            { answerText: 'Norway', isCorrect: false },
+            { answerText: 'Sweden', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'What is the best-selling book series of the 21st century?',
+        answerOptions: [
+            { answerText: 'Twilight', isCorrect: false },
+            { answerText: 'The Hunger Games', isCorrect: false },
+            { answerText: 'The Girl with Dragon Tattoo', isCorrect: false },
+            { answerText: 'Harry Potter', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'What was the clothing company Nike originally called?',
+        answerOptions: [
+            { answerText: 'Puma', isCorrect: false },
+            { answerText: 'Blue Ribbon Sports', isCorrect: true },
+            { answerText: 'Do It Sports', isCorrect: false },
+            { answerText: 'The Tick', isCorrect: false },
+        ],
+    },
+];
+
+export default questions;
